96da2f28739e8376995b811a9835456d7ce43f9e
boczektomasz@gmail.com <boczektomasz@gmail.com>
2021-12-06 10:25:30 +0100

    git

--- src/main/java/com/mycompany/app/services/ProducerService.java
+++ src/main/java/com/mycompany/app/services/ProducerService.java
@@ -6,11 +6,20 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 
 public class ProducerService {
+
+    // parametrization of variables declared in resources/application.properties:
+    @Value("${BOOTSTRAP_SERVERS}")
+    private static String bootstrap_servers;
+
+    @Value("${TOPIC_NAME}")
+    private static String topic_name;
+
     public static void produce(String message) {
         // When Kafka running locally in Docker use "127.0.0.1:9092"
-        String bootstrapServer = "172.21.202.252:9092,172.21.83.196:9092,172.21.194.161:9092";
+        String bootstrapServer = bootstrap_servers;
 
         // create producer properties
         Properties props = new Properties();
@@ -22,7 +31,7 @@ public class ProducerService {
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
 
         // create producer record
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", message);
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic_name, message);
 
         // send data - async
         producer.send(record);
